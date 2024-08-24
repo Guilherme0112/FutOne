0d4b23420ec5152968493dd2a0598d72087e418e
Guilherme0112 <guimendesmen124@gmail.com>
2024-08-24 12:43:36 -0300

    Curso Node

--- index.js
+++ index.js
@@ -5,16 +5,42 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
+
+    // Criando arquivo
+
     fs.readFile('demo.html', function(err, data){
         res.write(data);
     });
+
+    // Adicionando texto no arquivo
+
     fs.appendFile('cursoNode.txt', 'Curso de Noje JS', function(err){
         if(err) throw err;
         console.log('Salvo!');    
     });
+
+
+    // Atualizando o texto do arquivi 
+
     fs.writeFile('cursoNode.txt', 'Este é um arquivo de texto adicionado com Node.JS', function(err){
         if(err) throw err;
         console.log('Atualizado com sucesso!')
     });
+
+
+    // Deletado o arquivo
+
+    // fs.unlink('cursoNode.txt', function(err){
+    //     if(err) throw err;
+    //     console.log('Apagado!');
+    // })
+
+
+    // Renomeando o arquivo
+
+    fs.rename('cursoNode.txt', 'CursoNodeJS.txt', function(err){
+        if(err) throw err;
+        console.log('Renomeado!')
+    })
     res.end('O horário atual é ' + mo.myDateTime());
 }).listen(8080);
