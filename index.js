52d61cb01d8a9f748e2ab937b5bc6303bbd475bc
Guilherme0112 <guimendesmen124@gmail.com>
2024-08-24 11:17:47 -0300

    Curso Node

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var http = require('http');
+var mo = require('./demo.js');
+var fs = require('fs');
+
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
+    fs.readFile('demo.html', function(err, data){
+        res.write(data);
+    });
+    fs.appendFile('cursoNode.txt', 'Curso de Noje JS', function(err){
+        if(err) throw err;
+        console.log('Salvo!');    
+    });
+    fs.writeFile('cursoNode.txt', 'Este é um arquivo de texto adicionado com Node.JS', function(err){
+        if(err) throw err;
+        console.log('Atualizado com sucesso!')
+    });
+    res.end('O horário atual é ' + mo.myDateTime());
 }).listen(8080);
