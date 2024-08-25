31691f82b103e18dfdf0fcbf7faef5964a6c5e63
Guilherme0112 <guimendesmen124@gmail.com>
2024-08-25 20:03:49 -0300

    Curso Node

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 var http = require('http');
 var con = require('./database/db_connection');
+var formidable = require('formidable');
+const { Console } = require('console');
 
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        con.query('SELECT * FROM produtos', (err, results) => {
-            if(err) throw err;
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+
+    // Selecionar os dados do banco de dados
+
+    if(req.url === '/lista'){
+        con.query("SELECT * FROM produtos", (err, results) => {
+            if(err){
+                res.write(JSON.stringify({"Erro": "Erro ao buscar os dados."}));
+            }
             if(Object.keys(results).length === 0){
-                res.write('Sem dados por agora');
+                res.write(JSON.stringify({"dados": "Sem dados"}));
                 return false;
             }
-            results.forEach(result => {
-                valor = result.valor;
-                valorString = valor.toString().replace('.', ',');
-                valor = parseFloat(valorString);
-                
-                var html = `<p>Id: ${result.id}</p>
-                            <p>Produto: ${result.produto}</p>
-                            <p>Estoque: ${result.estoque}</p>
-                            <p>Preço: ${valor}</p>`;
-                        
-                res.write(html);
+            res.write(JSON.stringify(results));
+            res.end();
+        });
+
+    // Adicionar dados ao banco de dados
+
+    } else if(req.url === '/add' && req.method === 'POST'){
+        try{    
+            const form = new formidable.IncomingForm();
+            form.parse(req, (err, dados, files) => {
+                if(err){
+                    res.write(JSON.stringify({"Erro: ": err}))
+                }
+                if(dados.hasOwnProperty('produto', 'valor', 'preco')){
+
+                    // Converção dos dados
+
+                    dados.produto = dados.produto.toString();
+                    dados.preco = parseFloat(dados.preco);
+                    dados.estoque = parseInt(dados.estoque);
+
+                    // Validação dos dados
+
+                    if(isNaN(dados.preco)){
+                        res.write(JSON.stringify({"Erro": "O valor do preço é inválido"}))
+                    }
+                    if(isNaN(dados.estoque)){
+                        res.write(JSON.stringify({"Erro": "O valor do estoque é inválido."}))
+                    }
+                    if(dados.produto.length > 50){
+
+                        res.write(JSON.stringify({"Erro": "O nome do produto deve ter menos que 50 caracteres."}));
+                    }
+                }
+                res.end();
             });
-            res.end()
-    });
+        } catch (err){
+            console.log(err);
+        }
+    }
 }).listen(8080);
