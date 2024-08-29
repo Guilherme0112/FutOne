98b50867c9524dc960b33f7279af7a05622a77ee
Guilherme0112 <guimendesmen124@gmail.com>
2024-08-29 12:36:24 -0300

    V1.0

--- database/db_connection.js
+++ database/db_connection.js
@@ -4,7 +4,7 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "curso-node",
+    database: "futone",
 });
 
 // con.connect(function(err){
