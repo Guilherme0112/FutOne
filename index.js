6457f73861a1e0db359bc718177ee45acce506da
Guilherme0112 <guimendesmen124@gmail.com>
2024-08-29 23:27:34 -0300

    V1.0

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
-
+var path = require('path')
 app.use(express.static('public'));
-var path = require('path');
 
 app.all('/', function(req, res){
     res.sendFile(__dirname + '/public/pages/index.html');
 })
+
+// Login
+
+app.get('/login', function(req, res){
+    res.sendFile(__dirname + '/public/pages/login.html');
+});
+
 app.listen(8080, () => {
     console.log('Executando')
 })
