15a6ae3cc00156df905d8bb8097fa7c8231cc63c
Guilherme0112 <guimendesmen124@gmail.com>
2024-08-31 12:03:07 -0300

    V1.0

--- index.js
+++ index.js
@@ -2,30 +2,48 @@ var express = require('express');
 var app = express();
 var path = require('path')
 var con = require('./database/db_connection');
+var ejs = require('ejs');
+var route = express.Router();
 app.use(express.static('public'));
 
-app.all('/', function(req, res){
-    res.sendFile(__dirname + '/public/pages/index.html');
+// config ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public/views'));
 
+app.get('/', function(req, res){
+    con.query("SELECT * FROM postagens LIMIT 10", (err, dados) => {
+        if(err){
+            console.log("Erro: " + err);
+        } else{
+            // console.log(dados);
+            res.render('index', {posts: dados})
+        }
+    })
 })
 
-// Login
+// Login e Registro
 
 app.get('/login', function(req, res){
-    res.sendFile(__dirname + '/public/pages/login.html');
-    con.query("SELECT * FROM postagens LIMIT 10", (err, result) => {
+    res.render('login');
+});
+
+app.get('/register', function(req, res){
+    res.render('register');
+});
+
+// Aba de exibição de posts
+
+app.get('/post/:id', function(req, res) {
+    var idPost = req.params.id;
+    con.query("SELECT * FROM postagens WHERE id = " + idPost + " LIMIT 1", (err, dados) => {
         if(err){
-            console.log(err)
-            res.status(500).send("Erro ao buscar dados");
+            throw err;
         } else {
-            console.log(typeof result)
+            res.render('post', {post: dados});
         }
     });
 });
 
-app.get('/register', function(req, res){
-    res.sendFile(__dirname + '/public/pages/register.html');
-});
 
 app.listen(8080, () => {
     console.log('Executando')
--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-fetch('/')
-    .then(response => response.json())
-    .then(dados => {
-        const html = document.querySelector('#main');
-        dados.forEach(dado => {
-            
-        });
-    });
